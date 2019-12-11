ce0942d10fded0897818dbf77a55663e41a6089f
relicz <47209470+relicz@users.noreply.github.com>
2019-12-10 22:01:14 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ TOKEN = "TOKENHERE"
 REDDIT_ID = 'scriptidhere'
 REDDIT_SECRET = 'secrethere'
 USER_AGENT = 'useragenthere(google to find it)'
+DB_IP = ''
+DB_USER = ''
+DB_PASSWORD = ''
+DB_NAME = ''
